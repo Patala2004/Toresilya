dec6361532bd08c5423d5fc906fa475f827b9bb2
DisableGraphics <elchifladod@gmail.com>
2024-10-01 18:19:42 +0200

    animaciones slime

--- Assets/Resources/Enemigo/scripts/slime.cs
+++ Assets/Resources/Enemigo/scripts/slime.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class slime : enemigo
@@ -8,19 +9,22 @@ public class slime : enemigo
 	public float persecutionRadius = 5.0f;
 
 	public float jumpForce = 120f, jumpTimerInit = 1f, jumpTimer;
+	private Animator ac;
 
     // Start is called before the first frame update
     new void Start()
     {
         base.Start();
 		jumpTimer = jumpTimerInit;
+		ac = GetComponent<Animator>();
     }
     private void FixedUpdate()
     {
 		float magnitude = (jugador.gameObject.transform.position - gameObject.transform.position).magnitude;
-		
         if(magnitude <= persecutionRadius) {
+			ac.SetBool("detectedPlayer", true);
 			jumpTimer -= Time.fixedDeltaTime;
+			
 			if(jumpTimer <= 0f) {
 				Vector2 dir = (jugador.transform.position - gameObject.transform.position).normalized;
 				rb.AddForce(dir*jumpForce);
@@ -28,6 +32,7 @@ public class slime : enemigo
 				jumpTimer = jumpTimerInit;
 			}
 		} else {
+			ac.SetBool("detectedPlayer", false);
 			jumpTimer = jumpTimerInit;
 		}
     }
