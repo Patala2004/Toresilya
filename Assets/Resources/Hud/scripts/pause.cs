03ace1a78b481e0d6aba4e1440f66a23f6c7fc64
Christian <christian.most@alumnos.upm.es>
2024-11-06 02:08:36 +0100

    Añadido post processing blurr para cuando se está en pausa

--- Assets/Resources/Hud/scripts/pause.cs
+++ Assets/Resources/Hud/scripts/pause.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class pause : MonoBehaviour
 {
 
     [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private Volume blurrVolume;
 
     public bool isPaused = false;
     // Start is called before the first frame update
@@ -20,11 +22,13 @@ public class pause : MonoBehaviour
             if(!isPaused){
                 Time.timeScale = 0;
                 pauseMenu.SetActive(true);
+                blurrVolume.enabled = true;
                 isPaused = true;
             }
             else{
                 Time.timeScale = 1;
                 pauseMenu.SetActive(false);
+                blurrVolume.enabled = false;
                 isPaused = false;
             }            
         }
@@ -37,6 +41,7 @@ public class pause : MonoBehaviour
     public void unPause(){
         Time.timeScale = 1;
         pauseMenu.SetActive(false);
+        blurrVolume.enabled = false;
         isPaused = false;
     }
 }
