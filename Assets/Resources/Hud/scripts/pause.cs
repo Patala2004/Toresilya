abfac1b083a3e33a285ed567708b0189139cace0
Christian <christian.most@alumnos.upm.es>
2024-11-06 01:00:38 +0100

    El juego se pausa con ESC y cambiado el cursor a que cambie el cursor en vez de ser un gamobject que te sigue

---
+++ Assets/Resources/Hud/scripts/pause.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pause : MonoBehaviour
+{
+
+    [SerializeField] private GameObject pauseMenu;
+
+    public bool isPaused = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            if(!isPaused){
+                Time.timeScale = 0;
+                pauseMenu.SetActive(true);
+                isPaused = true;
+            }
+            else{
+                Time.timeScale = 1;
+                pauseMenu.SetActive(false);
+                isPaused = false;
+            }            
+        }
+    }
+}
