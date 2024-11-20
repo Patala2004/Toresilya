9da2cca7c844b8e9426c211d49ff1c1dfecb02b7
Patala2004 <christian.most@hotmail.com>
2024-11-20 15:54:56 +0100

    hola

--- Assets/Resources/Items/Script/Items/Item_1.cs
+++ Assets/Resources/Items/Script/Items/Item_1.cs
@@ -7,10 +7,14 @@ public class Item_1 : Item
     //Declaramos stats o cosas que modificará el item
 
     // Start is called before the first frame update
+
+    GameObject rayo;
+
     void Start()
     {
         player = GameObject.Find("player").GetComponent<Player>();
         descriptionItem = "Lanza un rayo cuando atacas a un enemigo";
+        rayo = Instantiate(Resources.Load<GameObject>("Items/Prefabs efectos/rayo"));
     }
 
     // Update is called once per frame
@@ -22,6 +26,7 @@ public class Item_1 : Item
     public override void grabItem(Player p)
     {
         base.grabItem(p);
+        player.attackMechanics.Add(lanzarRayo);
  
     }
 
@@ -30,7 +35,7 @@ public class Item_1 : Item
             // Lanzar rayo al enemigo
             
             enemy.TakeDamage(UnityEngine.Random.Range(player.sword.attackDamage[0], player.sword.attackDamage[1]) * multDanoRayo, player.ang, player.sword.attackKnockback * (player.sword.knockbackMultiplicator<0? 0:player.sword.knockbackMultiplicator));
-
+            rayo.transform.position = enemy.gameObject.transform.position;
             // Poner estados
             if(Random.Range(0f,1f) < probCaosRayo){
                 // Poner estado caos
@@ -43,6 +48,11 @@ public class Item_1 : Item
             // ...
 
         }
+    }
+
+    public void OnTriggerEnter2D(){
+        grabItem(player);
+        Destroy(this);
     }
 
 }
