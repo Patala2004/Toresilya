6c15658b321cdb156d48b7d2f7f77e5b3c3121f2
L4amda <alvaro572004@gmail.com>
2024-11-20 11:46:49 +0100

    Item 18

---
+++ Assets/Resources/Items/Script/Items/Item_18.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item_18 : Item
+{
+    //Declaramos stats o cosas que modificara el item
+    public float addDamage = 0.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Declaracion del item
+        player = GameObject.Find("player").GetComponent<Player>();
+        descriptionItem = "";
+        descripcionRecoger = "";
+        nombre = "";
+        unique = false;
+        rarity = "";
+        precio = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void grabItem(Player p)
+    {
+        base.grabItem(p);
+        //ANADIMOS LAS STATS O LO QUE HAGA EL ITEM
+        p.enterRoomMechanics.Add(damageUpWhenEnteringRoom);
+
+
+    }
+
+    public void damageUpWhenEnteringRoom()
+    {
+        player.sword.dmgMultiplicator += addDamage;
+        StartCoroutine(rutinaEntrar());
+    }
+
+    public IEnumerator rutinaEntrar(){
+    {
+            yield return new WaitForSeconds(5f);
+            player.sword.dmgMultiplicator-= addDamage;
+    }
+
+
+
+}
