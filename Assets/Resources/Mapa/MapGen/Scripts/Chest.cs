a718970e191c841a71d3ea86a9dda22e1dc42893
Christian <christian.most@alumnos.upm.es>
2024-11-24 12:32:57 +0100

    Ahora los cofres se abren de un golpe en vez de con colision

--- Assets/Resources/Mapa/MapGen/Scripts/Chest.cs
+++ Assets/Resources/Mapa/MapGen/Scripts/Chest.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Chest : MonoBehaviour
+public class Chest : Golpeable
 {
 
     [SerializeField] private GameObject BoxSprite;
     [SerializeField] private GameObject itemHolder;
+
+    private BoxCollider2D col;
     bool hasBeenOpened = false;
 
     private ItemGenerator itemGen;
@@ -14,6 +16,7 @@ public class Chest : MonoBehaviour
     void Start()
     {
         itemGen = GameObject.Find("ItemGenerator").GetComponent<ItemGenerator>();
+        col = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -22,8 +25,13 @@ public class Chest : MonoBehaviour
         
     }
 
-    public void OnTriggerEnter2D(){
-        if(hasBeenOpened) return;
+    public override void TakeDamage(float damage)
+    {
+        OpenChest();
+    }
+
+    public void OpenChest(){
+        
         GameObject newItem = itemGen.getItem();
         newItem.transform.SetParent(this.transform, false);
         newItem.transform.localPosition = new Vector2(0,0);
@@ -32,6 +40,9 @@ public class Chest : MonoBehaviour
         itemHolder.SetActive(true);
         itemHolder.GetComponent<ChestTile>().SetText(newItem.GetComponent<Item>().descriptionItem);
         itemHolder.GetComponent<ChestTile>().item = newItem.GetComponent<Item>();
+        col.enabled = false;
+
+        this.enabled = false;
         
     }
 }
