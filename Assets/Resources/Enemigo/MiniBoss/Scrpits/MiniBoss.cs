9bb5833ae2b71cc4fb30347173dabcad1ec4fe4d
Zumooo <nicolas.cidoncha@alumnos.upm.es>
2024-12-30 02:15:07 +0100

    MiniBoss

---
+++ Assets/Resources/Enemigo/MiniBoss/Scrpits/MiniBoss.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniBoss : Enemy
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        base.Start();
+    }
+    new void FixedUpdate()
+    {
+        base.FixedUpdate();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        base.Update();
+    }
+}
