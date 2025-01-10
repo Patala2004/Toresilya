a4a9eabf0b04809379d3bee61536fb20f0bade42
L4amda <alvaro572004@gmail.com>
2025-01-10 23:41:29 +0100

    CASI TODOS LOS ITEMS HECHOS AHHHHHHHHHHHHHH
    UWU

---
+++ Assets/Resources/Items/Script/Items/Item_231.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item_231 : Item
+{
+    //Declaramos stats o cosas que modificara el item
+
+    //+200% del base
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Declaracion del item
+        player = GameObject.Find("player").GetComponent<Player>();
+        descriptionItem = "Item 230";
+        descripcionRecoger = "";
+        nombre = "";
+        unique = false;
+        rarity = "";
+        precio = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void grabItem(Player p)
+    {
+        base.grabItem(p);
+        //ANADIMOS LAS STATS O LO QUE HAGA EL ITEM
+            p.sword.attackSpeed = 0.1f;
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        // Verificar si el objeto con el que colisionamos es el jugador
+        if (other.gameObject.CompareTag("player"))
+        {
+            grabItem(player);
+            Debug.Log("TUS MUERTOS");
+            transform.position = new Vector3(10000, 100000, transform.position.z);// Destruir el objeto después de recogerlo
+        }
+    }
+}
---
+++ Assets/Resources/Items/Script/Items/Item_232.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item_232 : Item
+{
+    //Declaramos stats o cosas que modificara el item
+    float masVelMov = 0.2f; //20%
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Declaracion del item
+        player = GameObject.Find("player").GetComponent<Player>();
+        descriptionItem = "";
+        descripcionRecoger = "";
+        nombre = "Item 232";
+        unique = false;
+        rarity = "";
+        precio = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void grabItem(Player p)
+    {
+        base.grabItem(p);
+        //ANADIMOS LAS STATS O LO QUE HAGA EL ITEM
+
+        p.velWalk = p.velWalk + (masVelMov * p.velWalk);
+
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        // Verificar si el objeto con el que colisionamos es el jugador
+        if (other.gameObject.CompareTag("player"))
+        {
+            grabItem(player);
+            Debug.Log("TUS MUERTOS");
+            transform.position = new Vector3(10000, 100000, transform.position.z);// Destruir el objeto después de recogerlo
+        }
+    }
+}
