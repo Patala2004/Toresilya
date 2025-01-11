a75c48f2c8de3dee6a4e3ae6fedfd1681901b16a
L4amda <alvaro572004@gmail.com>
2025-01-11 01:42:48 +0100

    FEIN FEIN FEIN

--- Assets/Resources/Items/Script/Items/Item_67.cs
+++ Assets/Resources/Items/Script/Items/Item_67.cs
@@ -14,7 +14,7 @@ public class Item_67 : Item
         player = GameObject.Find("player").GetComponent<Player>();
         descriptionItem = "1223";
         descripcionRecoger = "";
-        nombre = "Item 66";
+        nombre = "Item 67";
         unique = false;
         rarity = "";
         precio = 0;
@@ -50,15 +50,6 @@ public class Item_67 : Item
             }
         }
     }
-    public void OnTriggerEnter2D(Collider2D other)
-    {
-        // Verificar si el objeto con el que colisionamos es el jugador
-        if (other.gameObject.CompareTag("player"))
-        {
-            grabItem(player);
-            Debug.Log("TUS MUERTOS");
-            transform.position = new Vector3(10000, 100000, transform.position.z);// Destruir el objeto después de recogerlo
-        }
-    }
+   
 
 }
