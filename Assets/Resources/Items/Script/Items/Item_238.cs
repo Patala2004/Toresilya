9257388db193a46dca5f7e75e4f3803772ad36cd
L4amda <alvaro572004@gmail.com>
2025-01-11 19:54:04 +0100

    2 items mas

---
+++ Assets/Resources/Items/Script/Items/Item_238.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item_238 : Item
+{
+    //Declaramos stats o cosas que modificara el item
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Declaracion del item
+        player = GameObject.Find("player").GetComponent<Player>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void grabItem(Player p)
+    {
+        base.grabItem(p);
+        //ANADIMOS LAS STATS O LO QUE HAGA EL ITEM
+
+        p.healthMax += 5f;
+        p.health += 5f;
+
+
+    }
+}
---
+++ Assets/Resources/Items/Script/Items/Item_239.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item_239 : Item
+{
+    //Declaramos stats o cosas que modificara el item
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Declaracion del item
+        player = GameObject.Find("player").GetComponent<Player>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void grabItem(Player p)
+    {
+        base.grabItem(p);
+        //ANADIMOS LAS STATS O LO QUE HAGA EL ITEM
+
+        p.resistanceMax += 5f;
+        p.resistance += 5f;
+
+
+    }
+}
