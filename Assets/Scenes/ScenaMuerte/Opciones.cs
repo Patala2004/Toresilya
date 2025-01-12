3b1e2e056759be7977d5a4d0f493d719ef3d7e04
Zumooo <nicolas.cidoncha@alumnos.upm.es>
2025-01-12 16:47:59 +0100

    Arreglo menu pausa

--- Assets/Scenes/ScenaMuerte/Opciones.cs
+++ Assets/Scenes/ScenaMuerte/Opciones.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class Opciones : MonoBehaviour
 {
     AudioSource aS;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +22,7 @@ public class Opciones : MonoBehaviour
     public void Reiniciar()
     {
         SceneManager.LoadScene("SampleScene");
+
     }
     public void Menu()
     {
